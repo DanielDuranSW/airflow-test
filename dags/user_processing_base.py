b8a8be9971bb87fd120a9adae3aa0775a48d9679
Daniel Durán Fernández <daniel.duran.sw@gmail.com>
2024-09-26 12:16:48 +0200

    base airflow test DONE
    Problems with SSL

--- dags/user_processing_base.py
+++ dags/user_processing_base.py
@@ -2,6 +2,6 @@ from airflow import DAG
 
 from datetime import datetime
 
-with DAG('user_procesing', start_date=datetime(2022, 1, 1),
+with DAG('user_processing', start_date=datetime(2022, 1, 1),
         schedule_interval='@daily', catchup=False) as dag:
     None
